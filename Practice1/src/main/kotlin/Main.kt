bf642d94a34c2993f27def683fec8183a264cb87
vjkbhu <sheblanovnikita@gmail.com>
2023-09-06 19:34:03 +0300

    Practika

---
+++ Practice1/src/main/kotlin/Main.kt
@@ -0,0 +1,51 @@
+fun main(args: Array<String>) {
+//задача 1
+    val  exercises : Int = 13
+    var exercisesSolved : Int =  0
+//---------------------------------
+
+//задача 2
+val  exercises : Int = 18
+//---------------------------------
+
+//задача 3
+ var exercisesSolved : Double = 18.0
+    exercisesSolved = (exercisesSolved + 30)/2
+//---------------------------------
+
+//задача 4
+ val testNumber : Int = 26
+    val evenOdd
+    testNumber % 2
+    testNumber = evenOdd
+//---------------------------------
+
+// задача 5
+var answer : Int = 0
+    answer+=1
+    (answer+10)*10 shr 3
+//---------------------------------
+
+//задача 6
+val
+//---------------------------------
+
+//задача 7
+1-4610
+2-3600
+3-4600
+//---------------------------------
+
+//задача 8
+    (5*3)-(4/(2*2))
+//--------------------------------
+
+
+//задача 9
+ val  a : Double = 2.4
+ val  b : Double = 2.7
+    val awerage = (a +b) / 2
+//---------------------------------
+
+
+}
